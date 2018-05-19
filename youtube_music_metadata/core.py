f87412ec575ae5f8da94259426c31c45a980f44f
Naresh <ghostwriternr@gmail.com>
2018-05-19 13:48:04 +0530

    Fetch embedded metadata

---
+++ youtube_music_metadata/core.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+REQUESTS_SESSION = requests.Session()
+
+def youtube_attached_metadata(song_link):
+    yt_data = {}
+    yt_page = REQUESTS_SESSION.get(song_link).text
+    soup = BeautifulSoup(yt_page, 'html.parser')
+    yt_title = soup.find('meta', {'property': 'og:title'}).get('content').strip()
+    yt_data['title'] = yt_title
+    yt_metadata = soup.find('div', id='watch-description-extras').find(
+        'ul', {'class': 'watch-extras-section'}).find_all('li', {'class': 'watch-meta-item'})
+    for metadata in yt_metadata:
+        if metadata.find('h4', {'class': 'title'}).get_text().strip() == 'Song':
+            song_name = metadata.find('ul', {'class': 'content'}).get_text().strip()
+            yt_data['song'] = song_name
+        elif metadata.find('h4', {'class': 'title'}).get_text().strip() == 'Artist':
+            artist_name = metadata.find('ul', {'class': 'content'}).get_text().strip()
+            yt_data['artist'] = artist_name
+        elif metadata.find('h4', {'class': 'title'}).get_text().strip() == 'Album':
+            album_name = metadata.find('ul', {'class': 'content'}).get_text().strip()
+            yt_data['album'] = album_name
+    return yt_data
+
+def get_metadata(song_link, spotify=False, musixmatch=False):
+    yt_data = youtube_attached_metadata(song_link)
+    print(yt_data)
+
+if __name__ == "__main__":
+    song_link = "https://www.youtube.com/watch?v=zwHF2y2ceSc&feature=share"
+    get_metadata(song_link)
